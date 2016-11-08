c252dd3083f9bbec3423b36ec788b93b6fa015a3
Antonio Aguilar <jaguilar992@gmail.com>
2016-11-08 16:52:49 -0600

    First

---
+++ test/servidorhtml/Regexp.java
@@ -0,0 +1,36 @@
+package servidorhtml;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Antonio
+ */
+public class Regexp {
+    //private static final String REGEXP = "([^A-Z\\s+].+[^\\s+HTTP/\\d+\\.\\d+])";
+    private static final String REGEXP = "([^A-Z\\s+].*[^HTP/\\d+\\.\\d+])";
+    private static final String REGEXP2 = "(^[A-Z]*)";
+    public static String get_filename(String line){
+	Pattern p = Pattern.compile(Regexp.REGEXP);
+	Matcher m = p.matcher(line);
+	
+	if (m.find()) {
+	    return m.group().replace(" ", "");
+	}else{
+	    return null;
+	}
+    }
+    
+    public static String get_method(String line){
+	Pattern p = Pattern.compile(Regexp.REGEXP2);
+	Matcher m = p.matcher(line);
+	
+	if (m.find()) {
+	    return m.group().replace(" ", "");
+	}else{
+	    return null;
+	}
+    }
+}
+
